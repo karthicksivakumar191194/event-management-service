0c041135d03d8758e551cb35014e8e512286ae90
Karthick Sivakumar <karthik@zaigoinfotech.com>
2020-03-14 13:57:08 +0530

    file & code restructure, dotenv, nodemon, mongoose setup

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//mongoose
+const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+/* App Routes Start */
+
+/* App Routes End */
 
 var app = express();
+
+//Mongoose DB Connection
+/* mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to database')) */
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +29,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// response headers
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");        
+    next();
+});
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+/* App Routes Start */
+
+/* App Routes End */
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
