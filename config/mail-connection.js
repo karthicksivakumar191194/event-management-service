33737a8344f96c7dcd7a8f51a56d648925c758cb
Karthick Sivakumar <karthik@zaigoinfotech.com>
2020-03-21 19:09:52 +0530

    Mail - NodeMailer Config & Connection Testing

---
+++ config/mail-connection.js
@@ -0,0 +1,22 @@
+var nodeMailer = require('nodemailer');
+
+let transporter = nodeMailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: process.env.SMTP_USERNAME,
+        pass: process.env.SMTP_PASSWORD
+    }
+});
+
+exports.sendMail = function (mailOptions) {
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            return false;
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+        return true;
+    });
+}
--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const Mail = require('../config/mail-connection')
 
 exports.index = async function(req, res, next) {
    try {
@@ -22,8 +23,24 @@ exports.save = async function(req, res, next) {
      postalCode: req.body.postalCode
    })
    
+   var mailBody = `
+            <p>Hi Test,</p>
+            <p>You have been added to the ${process.env.SITE_NAME}, please find below your login credentials.</p>
+            <p>
+              <strong>Email: </strong>test@test.com<br>
+              <strong>Password: </strong>test123</p>
+          `;
+
+   let mailOptions = {
+    from: '"Karthick Sivakumar" <test@gmail.com>', 
+    to: 'karthik@zaigoinfotech.com', 
+    subject: 'Event Management | New User Account Notification', 
+    html: mailBody 
+  };
+
    try {
      const newUser = await user.save()
+     Mail.sendMail(mailOptions)
      res.status(201).json(newUser)
    } catch (err) {
      res.status(400).json({ message: err.message })
